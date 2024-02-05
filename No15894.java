5c4f65847fd094d5ea09a0bb0f6967d0fa247410
sirinyeolung <143485496+sirinyeolung@users.noreply.github.com>
2024-02-05 23:32:15 +0900

    No15894 answer

---
+++ No15894.java
@@ -0,0 +1,17 @@
+package baekjooon10;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class No15894 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		BigInteger N = sc.nextBigInteger();
+
+		System.out.println(N.multiply(BigInteger.valueOf(4)));
+	}
+
+}
